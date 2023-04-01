d6716e97f7daa60f8cb7fed3de7f2758111b1de8
dagregi <dagim6066@gmail.com>
2023-04-01 14:00:50 +0300

    Initialized the app

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{tsx,ts,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      prefix: 'cat',
+      defaultFlavour: 'mocha',
+    })
+  ],
+}
+
