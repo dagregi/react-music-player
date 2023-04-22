5eb517afd9a99e53bccf4bd9fb24a4024196c34f
dagregi <120724828+dagregi@users.noreply.github.com>
2023-04-22 16:03:48 +0300

    Made the header fixed on all pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import SideBar from "./SideBar";
 
 const Header = () => {
   return (
-    <article className="relative w-full top-0 bg-cat-base/20 flex">
+    <header className="fixed w-full top-0 bg-cat-base/0 flex">
       <SideBar />
       <h1 className="bg-clip-text mx-auto text-transparent p-2 text-center font-mono text-xl bg-gradient-to-br from-cat-pink to-cat-mauve">
         Placeholder
       </h1>
-    </article>
+    </header>
   );
 };
 
